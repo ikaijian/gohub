29a9f85dcca7688f4259c5e33ce4683f979ef39a
ikaijian <ikaijian.163.com>
2024-12-11 10:33:20 +0800

    用户模型

---
+++ app/models/user/user_util.go
@@ -0,0 +1,18 @@
+package user
+
+import "gohub/pkg/database"
+
+// IsEmailExist 判断 Email 已被注册
+func IsEmailExist(email string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("email = ?", email).Count(&count)
+
+	return count > 0
+}
+
+// IsPhoneExist 判断 phone【手机号】 已被注册
+func IsPhoneExist(phone string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("phone = ?", phone).Count(&count)
+	return count > 0
+}
