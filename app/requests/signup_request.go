3e059ead01701d1c6c02d4a860e83231522af469
ikaijian <ikaijian.163.com>
2024-12-17 13:32:11 +0800

    验证请求

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -9,6 +9,11 @@ type SignupPhoneExistRequest struct {
 	Phone string `json:"phone,omitempty" valid:"phone"`
 }
 
+type SignupEmailExistRequest struct {
+	Email string `json:"email,omitempty" valid:"email"`
+}
+
+// ValidateSignupPhoneExist 验证手机号
 func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
 	// 自定义验证规则
 	rules := govalidator.MapData{
@@ -23,6 +28,32 @@ func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]str
 		},
 	}
 
+	// 配置初始化
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
+		Messages:      messages,
+	}
+
+	return govalidator.New(opts).ValidateStruct()
+}
+
+// ValidateSignupEmailExist 验证邮箱
+func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
+	// 自定义验证规则
+	rules := govalidator.MapData{
+		"email": []string{"required", "min:4", "max:30", "email"},
+	}
+	// 自定义验证出错时的提示
+	messages := govalidator.MapData{
+		"email": []string{
+			"required:Email 为必填项",
+			"min:Email 长度需大于 4",
+			"max:Email 长度需小于 30",
+			"email:Email 格式不正确，请输入正确的邮箱格式",
+		},
+	}
 	// 配置初始化
 	opts := govalidator.Options{
 		Data:          data,
