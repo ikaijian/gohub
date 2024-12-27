b9b6235d163994fcd997c2b804744684fffce68b
ikaijian <ikaijian.163.com>
2024-12-27 10:34:27 +0800

    命令行模式

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+	"gohub/app/cmd"
 	"gohub/bootstrap"
 	btsConfig "gohub/config"
 	"gohub/pkg/config"
+	"gohub/pkg/console"
+	"os"
 )
 
 // 加载 config 目录下的配置信息
@@ -15,34 +18,45 @@ func init() {
 }
 
 func main() {
-	// 配置初始化，依赖命令行 --env 参数
-	var env string
-	flag.StringVar(&env, "env", "", "加载的配置文件 env 文件, 默认为 dev")
-	flag.Parse()
-	config.InitConfig(env)
-	// 初始化 Logger
-	bootstrap.SetupLogger()
-	// new 一个gin Engine 实例
+
+	// 应用的主入口，默认调用 cmd.CmdServe 命令
+	var rootCmd = &cobra.Command{
+		Use:   "Gohub",
+		Short: "A simple forum project",
+		Long:  `Default will run "serve" command, you can use "-h" flag to see all subcommands`,
+		// rootCmd 的所有子命令都会执行以下代码
+		PersistentPreRun: func(command *cobra.Command, args []string) {
+			// 配置初始化，依赖命令行 --env 参数
+			config.InitConfig(cmd.Env)
+
+			// 初始化 Logger
+			bootstrap.SetupLogger()
+
+			// 初始化数据库
+			bootstrap.SetupDB()
+
+			// 初始化 Redis
+			bootstrap.SetupRedis()
+
+			// 初始化缓存
+		},
+	}
+	// 注册子命令
+	rootCmd.AddCommand(
+		cmd.CmdServe,
+	)
+
+	// 配置默认运行 Web 服务
+	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
+
+	// 注册全局参数，--env
+	cmd.RegisterGlobalFlags(rootCmd)
+
 	router := gin.New()
-	// 初始化 DB
-	bootstrap.SetupDB()
-	// 初始化 Redis
-	bootstrap.SetupRedis()
 	// 初始化路由
 	bootstrap.SetupRoute(router)
-	// 测试 验证码
-	//verifycode.NewVerifyCode().SendSMS("18312839530")
-
-	// 设置 gin 的运行模式，支持 debug, release, test
-	// release 会屏蔽调试信息，官方建议生产环境中使用
-	// 非 release 模式 gin 终端打印太多信息，干扰到我们程序中的 Log
-	// 故此设置为 release，有特殊情况手动改为 debug 即可
-	gin.SetMode(gin.ReleaseMode)
-
-	// 运行服务
-	err := router.Run(":" + config.Get("app.port"))
-	if err != nil {
-		// 错误处理，端口被占用了或者其他错误
-		fmt.Println(err.Error())
+	// 执行主命令
+	if err := rootCmd.Execute(); err != nil {
+		console.Exit(fmt.Sprintf("Failed to run app with %v: %s", os.Args, err.Error()))
 	}
 }
