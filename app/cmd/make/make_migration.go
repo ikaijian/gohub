f43790206b9635bb1d8ebde26d8cbecc043c69aa
ikaijian <ikaijian.163.com>
2024-12-30 08:54:21 +0800

    话题模型和迁移

--- app/cmd/make/make_migration.go
+++ app/cmd/make/make_migration.go
@@ -9,9 +9,9 @@ import (
 
 var CmdMakeMigration = &cobra.Command{
 	Use:   "migration",
-	Short: "Create a migration file, example: make migration users add_users_table",
+	Short: "Create a migration file, example: make migration add_users_table  users",
 	Run:   runMakeMigration,
-	Args:  cobra.ExactArgs(1), // 只允许且必须传 1 个参数
+	Args:  cobra.ExactArgs(2), // 只允许且必须传 2 个参数
 }
 
 func runMakeMigration(cmd *cobra.Command, args []string) {
@@ -19,9 +19,10 @@ func runMakeMigration(cmd *cobra.Command, args []string) {
 	timeStr := app.TimenowInTimezone().Format("2006_01_02_150405")
 
 	model := makeModelFromString(args[0])
+	st := makeModelFromString(args[1])
 	fileName := timeStr + "_" + model.PackageName
 	filePath := fmt.Sprintf("database/migrations/%s.go", fileName)
 	//createFileFromStub(filePath, "migration", model, map[string]string{"{{FileName}}": fileName, "{{TableName}}": model.TableName})
-	createFileFromStub(filePath, "migration", model, map[string]string{"{{FileName}}": fileName})
+	createFileFromStub(filePath, "migration", st, map[string]string{"{{FileName}}": fileName})
 	console.Success("Migration file created，after modify it, use `migrate up` to migrate database.")
 }
